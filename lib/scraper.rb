a303b69190bb11e64e36f7f1f0e75e0dea2041f8
sunny-tier-6236 <taycurrie@gmail.com>
2020-04-29 22:45:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
 
-courses.each do |course|
-  puts course.text.strip
-end
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
